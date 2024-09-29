e3aa94f259e109ec510730b033660785153a16a9
Katherine Stehberg <kstehberg@gmail.com>
2024-09-29 18:09:10 -0300

    updated link product kit jardinería

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -6,7 +6,7 @@ const FeaturedProducts = () => {
       <h2>Productos Destacados</h2>
       <div className="featured-products-container">
         <div className="featured-product">
-          <img src="/vite-project/src/imagen/KitDeJardineria.jpg" alt="Kit de jardinería" />
+          <img src="https://biodiversidad.cl/wp-content/uploads/2024/09/kit-jardineria.jpg" alt="Kit de jardinería" />
           <h3>Kit de jardinería</h3>
           <p>Solo desde <span className="price">$9.99</span></p>
         </div>
