eec765b7d0beffa5dcee0bf824302319c7adfff8
niconstvc <nicol.villarreal@gmail.com>
2024-09-29 21:31:02 -0300

    update2130

---
+++ src/api.js
@@ -0,0 +1,35 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const fetchProducts = async () => {
+  try {
+    const response = await fetch(`${API_URL}/products`);
+    if (!response.ok) {
+      throw new Error('Error fetching products');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error; 
+  }
+};
+
+export const addProduct = async (product) => {
+  try {
+    const response = await fetch(`${API_URL}/products`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product),
+    });
+
+    if (!response.ok) {
+      throw new Error('Error adding product');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+};
